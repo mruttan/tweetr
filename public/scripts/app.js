650cb32e23992165253f5991eab295e3a33502f2
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-21 17:17:14 +0000

    loading tweets with ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,20 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from tweets.json
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+
+function loadTweets(){
+  $.ajax({
+    url: "/tweets",
+    method: 'GET',
+    success: function(tweets){
+      renderTweets(tweets)}
+  })
+
+}
 
 function renderTweets(tweets){
   for (let user in tweets){
@@ -62,14 +26,15 @@ function renderTweets(tweets){
 
 function createTweetElement(tweet){
   let $tweet = $('<article>').addClass('tweet');
-  let image = `<img class='avatar' src=${tweet['user']['avatars']['regular']}>`;
-  let userName = `<h2>${tweet['user']['name']}</h2>`;
-  let userAvatar = `<h4>${tweet['user']['handle']}</h4>`
+  let image = `<img class='avatar' src=${escape(tweet['user']['avatars']['regular'])}>`;
+  let userName = `<h2>${escape(tweet['user']['name'])}</h2>`;
+  let userAvatar = `<h4>${escape(tweet['user']['handle'])}</h4>`
+  let head = `<header>${image+userName+userAvatar}</header>`
   let tweetText = tweet['content']['text'];
-  let para = `<p class="tweet-text">${tweetText}</p>`
+  let para = `<p class="tweet-text">${escape(tweetText)}</p>`
   let timeStamp = printTime(tweet['created_at']);
-  let foot = `<footer><p class='tweet-time'>${timeStamp}</p></footer>`
-  $($tweet).append(`<header>${image+userName+userAvatar}</header>`)
+  let foot = `<footer><p class='tweet-time'>${escape(timeStamp)}</p></footer>`
+  $($tweet).append(head);
   $($tweet).append(para);
   $($tweet).append(foot);
   return $tweet[0].outerHTML;
@@ -108,9 +73,12 @@ function roundTime(time){
   return `${Math.floor(second)} seconds ago`;
 }
 
-
-
+function escape(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 $(document).ready(function(){
-  renderTweets(data);
+  loadTweets();
 })
