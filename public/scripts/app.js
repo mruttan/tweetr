755b5ff66e3623e5e509bc03c018a34689952b6d
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-21 17:52:27 +0000

    Refetch tweets on submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,21 @@
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 function loadTweets(){
   $.ajax({
     url: "/tweets",
     method: 'GET',
     success: function(tweets){
+      $('.tweet-contents').empty();
       renderTweets(tweets)}
   })
-
 }
 
 function renderTweets(tweets){
-  for (let user in tweets){
+  for (var user in tweets){
     tweetDOM = createTweetElement(tweets[user]);
     $('.tweet-contents').append(tweetDOM);
   }
@@ -25,15 +23,15 @@ function renderTweets(tweets){
 
 
 function createTweetElement(tweet){
-  let $tweet = $('<article>').addClass('tweet');
-  let image = `<img class='avatar' src=${escape(tweet['user']['avatars']['regular'])}>`;
-  let userName = `<h2>${escape(tweet['user']['name'])}</h2>`;
-  let userAvatar = `<h4>${escape(tweet['user']['handle'])}</h4>`
-  let head = `<header>${image+userName+userAvatar}</header>`
-  let tweetText = tweet['content']['text'];
-  let para = `<p class="tweet-text">${escape(tweetText)}</p>`
-  let timeStamp = printTime(tweet['created_at']);
-  let foot = `<footer><p class='tweet-time'>${escape(timeStamp)}</p></footer>`
+  var $tweet = $('<article>').addClass('tweet');
+  var image = `<img class='avatar' src=${escape(tweet['user']['avatars']['regular'])}>`;
+  var userName = `<h2>${escape(tweet['user']['name'])}</h2>`;
+  var userAvatar = `<h4>${escape(tweet['user']['handle'])}</h4>`
+  var head = `<header>${image+userName+userAvatar}</header>`
+  var tweetText = tweet['content']['text'];
+  var para = `<p class="tweet-text">${escape(tweetText)}</p>`
+  var timeStamp = printTime(tweet['created_at']);
+  var foot = `<footer><p class='tweet-time'>${escape(timeStamp)}</p></footer>`
   $($tweet).append(head);
   $($tweet).append(para);
   $($tweet).append(foot);
@@ -41,44 +39,70 @@ function createTweetElement(tweet){
 }
 
 function printTime(timeStamp){
-  let current = Date.now();
-  let diff = (timeStamp - current)/1000;
+  var current = Date.now();
+  var diff = (timeStamp - current)/1000;
   return roundTime(-1 * diff);
 }
 
 function roundTime(time){
-  let year = time/(365*24*60*60)
+  var year = time/(365*24*60*60)
   if(year > 1){
     return `${Math.floor(year)} years ago`;
   }
-  let month = time/(30*24*60*60);
+  var month = time/(30*24*60*60);
   console.log(month)
   if(month > 1){
     return `${Math.floor(month)} months ago`;
   }
-  let day = time/(24*60*60);
+  var day = time/(24*60*60);
   if(day > 1){
     return `${Math.floor(day)} days ago`;
   }
-  let hour = time/(60*60);
+  var hour = time/(60*60);
   console.log("hour",hour)
   if(hour > 1){
     return `${Math.floor(hour)} hours ago`;
   }
-  let minute = time/(60*60);
+  var minute = time/(60);
   if(minute > 1){
     return `${Math.floor(minute)} minutes ago`;
   }
-  let second = time/(60*60);
+  var second = time;
   return `${Math.floor(second)} seconds ago`;
 }
 
 function escape(str) {
-  let div = document.createElement('div');
+  var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
 $(document).ready(function(){
   loadTweets();
+  $('form').on('submit', function (event){
+    event.preventDefault();
+    $(this).children('.warning').remove();
+    var $text = $(this).children('textarea')[0];
+    var flashWarning;
+    if ($text.value === "" || $text.value === null){
+      flashWarning = "Please enter a tweet"
+    } else {
+      if ($text.length > 140){
+        flashWarning = "Tweets are limited to a length of 140 characters"
+      }
+    }
+    if (flashWarning){
+      $(this).append(`<p class="warning"> ${flashWarning} </p>`);
+    } else {
+      $.ajax({
+        url: "/tweets",
+        method: 'POST',
+        data: {text:$text.value},
+        success: function(){
+          loadTweets();
+          $text.value = "";
+        }
+      })
+    }
+  })
 })
