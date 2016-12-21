95b3e330c2cda11688c714120a89509d1d801c2d
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-21 03:03:25 +0000

    Implement Character Counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('form').children('textarea').on('input', function(){
+      let remainChar = 140 - this.value.length;
+      console.log(remainChar, this.value.length);
+      $(this).parent('form').children('.counter').html(remainChar);
+      if (remainChar < 0){
+        $(this).parent('form').children('.counter').addClass('invalid');
+      } else {
+      	$(this).parent('form').children('.counter').removeClass('invalid');
+      }
+  })
+})
